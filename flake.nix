f44225fbcb25eef6f65c09e4e4ea5eb975bfad44
Nikolay Nikolaev <xaknick@gmail.com>
2024-11-28 20:17:52 +0100

    Move to unstable branch

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "My Nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
