ed9efa632c5c7a5f0ac68da06124351508d351b5
Nikolay Nikolaev <xaknick@gmail.com>
2024-12-05 19:18:10 +0100

    Add UWSM supporting

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -1,9 +1,16 @@
 { pkgs, lib, ... }:
 {
+  # Enable cachix to avoid Hyprland recompilation
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   # Enable Hyprland
-  services.displayManager.defaultSession = "hyprland";
+  # services.displayManager.defaultSession = "hyprland";
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
@@ -15,6 +22,7 @@
     hypridle
     hyprpaper
     hyprshot
+    hyprpolkitagent
 
     # Required services or libraries
     qt6.qtwayland
