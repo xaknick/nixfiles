13caa14ce744c3ba3bc9b32d992e2ad2fdbe0c08
Nikolay Nikolaev <xaknick@gmail.com>
2024-12-20 21:52:18 +0100

    Qt5 compatibility by default

--- hosts/nixos/sddm.nix
+++ hosts/nixos/sddm.nix
@@ -4,5 +4,9 @@
     enable = true;
     wayland.enable = true;
     package = lib.mkForce pkgs.kdePackages.sddm; # qt6 sddm version
+    extraPackages = [
+      pkgs.kdePackages.qt5compat
+      pkgs.kdePackages.qtsvg
+    ];
   };
 }
